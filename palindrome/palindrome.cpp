845b4d692a13377d174aa7be46116f327e8a531e
Krishan Madan <krishanmadan@Krishans-MacBook-Pro.local>
2017-04-09 10:50:33 -0400

    first commit

---
+++ palindrome/palindrome.cpp
@@ -0,0 +1,45 @@
+#include<stdio.h>
+#include<iostream>
+#include <sstream>
+#include <ctype.h>
+#include <string.h>
+#include<typeinfo>
+#include<vector>
+#include <stdlib.h>
+#include<string>
+using namespace std;
+class Palindrome{
+  char* checkforpal;
+public:
+  Palindrome(char* x): checkforpal(x){}
+  bool palchecker()
+  {
+    printf("%s\n", checkforpal);
+    bool ispalindrome=true;
+    int i=0;
+    while(i<(strlen(checkforpal)))
+      {
+	if(checkforpal[i]!=checkforpal[(strlen(checkforpal)-1)-i])
+          {
+
+            ispalindrome=false;
+            break;
+          }
+        i++;
+      }
+    return ispalindrome;
+  }
+
+} ;
+
+int main(){
+  char *whatschecked=(char *)malloc(200);
+
+  cout<<"Enter a sentence";
+  //fgets(whatschecked, 200, stdin);                                          
+
+  scanf("[^\n]s", whatschecked);
+  Palindrome x(whatschecked);
+  cout<<x.palchecker();
+  return 0;
+}
